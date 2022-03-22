dd43a20937e88037a63161b28eedf71236ff9456
ItsCloudxo <ItsCloudxo@DESKTOP-469V821>
2022-03-22 18:45:02 +0100

    Add project files.

---
+++ Array_Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿string mystring = Console.ReadLine();
+
+int[] myarray = new int[mystring.Length];
+
+int i = 0;
+int sum = 0;
+
+while (i < mystring.Length)
+{
+    myarray[i] = int.Parse(mystring[i].ToString());
+    i++;
+}
+
+foreach (int num in myarray)
+{
+    Console.Write(num + ", ");
+}
+
+Array.ForEach(myarray, b => sum += b);
+
+
+Console.WriteLine(sum);
